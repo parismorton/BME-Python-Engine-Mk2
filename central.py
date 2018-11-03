a1db22d7aa1ceaecf6aac1eb3b8d3eca67d9a643
parismorton <42592885+parismorton@users.noreply.github.com>
2018-11-03 10:17:19 -0700

    Add files via upload

---
+++ central.py
@@ -0,0 +1,337 @@
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
+
+inputtedword = raw_input("Text-to-spell initialized. Type your word (6 letter limit).")
+print ("Spelling " + inputtedword + "...")
+
+
+
+
+import time
+time.sleep(1)
+if inputtedword[0] == alphabet[0]:
+	execfile("afile.py")
+elif inputtedword[0] == alphabet[1]:
+		execfile("bfile.py")
+elif inputtedword[0] == alphabet[2]:
+			execfile("cfile.py")
+elif inputtedword[0] == alphabet[3]:
+				execfile("dfile.py")
+elif inputtedword[0] == alphabet[4]:
+					execfile("efile.py")
+elif inputtedword[0] == alphabet[5]:
+						execfile("ffile.py")
+elif inputtedword[0] == alphabet[6]:
+							execfile("gfile.py")
+elif inputtedword[0] == alphabet[7]:
+								execfile("hfile.py")
+elif inputtedword[0] == alphabet[8]:
+									execfile("ifile.py")
+elif inputtedword[0] == alphabet[9]:
+										execfile("jfile.py")
+elif inputtedword[0] == alphabet[10]:
+											execfile("kfile.py")
+elif inputtedword[0] == alphabet[11]:
+												execfile("lfile.py")
+elif inputtedword[0] == alphabet[12]:
+													execfile("mfile.py")
+elif inputtedword[0] == alphabet[13]:
+														execfile("nfile.py")
+elif inputtedword[0] == alphabet[14]:
+															execfile("ofile.py")
+elif inputtedword[0] == alphabet[15]:
+																execfile("pfile.py")
+elif inputtedword[0] == alphabet[16]:
+																	execfile("qfile.py")
+elif inputtedword[0] == alphabet[17]:
+																		execfile("rfile.py")
+elif inputtedword[0] == alphabet[18]:
+																			execfile("sfile.py")
+elif inputtedword[0] == alphabet[19]:
+																				execfile("tfile.py")
+elif inputtedword[0] == alphabet[20]:
+																					execfile("ufile.py")
+elif inputtedword[0] == alphabet[21]:
+																						execfile("vfile.py")
+elif inputtedword[0] == alphabet[22]:
+																							execfile("wfile.py")
+elif inputtedword[0] == alphabet[23]:
+																								execfile("xfile.py")
+elif inputtedword[0] == alphabet[24]:
+																									execfile("yfile.py")
+elif inputtedword[0] == alphabet[25]:
+																										execfile("zfile.py")																				
+time.sleep(1)
+if inputtedword[1] == alphabet[0]:
+	execfile("afile.py")
+elif inputtedword[1] == alphabet[1]:
+		execfile("bfile.py")
+elif inputtedword[1] == alphabet[2]:
+			execfile("cfile.py")
+elif inputtedword[1] == alphabet[3]:
+				execfile("dfile.py")
+elif inputtedword[1] == alphabet[4]:
+					execfile("efile.py")
+elif inputtedword[1] == alphabet[5]:
+						execfile("ffile.py")
+elif inputtedword[1] == alphabet[6]:
+							execfile("gfile.py")
+elif inputtedword[1] == alphabet[7]:
+								execfile("hfile.py")
+elif inputtedword[1] == alphabet[8]:
+									execfile("ifile.py")
+elif inputtedword[1] == alphabet[9]:
+										execfile("jfile.py")
+elif inputtedword[1] == alphabet[10]:
+											execfile("kfile.py")
+elif inputtedword[1] == alphabet[11]:
+												execfile("lfile.py")
+elif inputtedword[1] == alphabet[12]:
+													execfile("mfile.py")
+elif inputtedword[1] == alphabet[13]:
+														execfile("nfile.py")
+elif inputtedword[1] == alphabet[14]:
+															execfile("ofile.py")
+elif inputtedword[1] == alphabet[15]:
+																execfile("pfile.py")
+elif inputtedword[1] == alphabet[16]:
+																	execfile("qfile.py")
+elif inputtedword[1] == alphabet[17]:
+																		execfile("rfile.py")
+elif inputtedword[1] == alphabet[18]:
+																			execfile("sfile.py")
+elif inputtedword[1] == alphabet[19]:
+																				execfile("tfile.py")
+elif inputtedword[1] == alphabet[20]:
+																					execfile("ufile.py")
+elif inputtedword[1] == alphabet[21]:
+																						execfile("vfile.py")
+elif inputtedword[1] == alphabet[22]:
+																							execfile("wfile.py")
+elif inputtedword[1] == alphabet[23]:
+																								execfile("xfile.py")
+elif inputtedword[1] == alphabet[24]:
+																									execfile("yfile.py")
+elif inputtedword[1] == alphabet[25]:
+																										execfile("zfile.py")
+time.sleep(1)
+if inputtedword[2] == alphabet[0]:
+	execfile("afile.py")
+elif inputtedword[2] == alphabet[1]:
+		execfile("bfile.py")
+elif inputtedword[2] == alphabet[2]:
+			execfile("cfile.py")
+elif inputtedword[2] == alphabet[3]:
+				execfile("dfile.py")
+elif inputtedword[2] == alphabet[4]:
+					execfile("efile.py")
+elif inputtedword[2] == alphabet[5]:
+						execfile("ffile.py")
+elif inputtedword[2] == alphabet[6]:
+							execfile("gfile.py")
+elif inputtedword[2] == alphabet[7]:
+								execfile("hfile.py")
+elif inputtedword[2] == alphabet[8]:
+									execfile("ifile.py")
+elif inputtedword[2] == alphabet[9]:
+										execfile("jfile.py")
+elif inputtedword[2] == alphabet[10]:
+											execfile("kfile.py")
+elif inputtedword[2] == alphabet[11]:
+												execfile("lfile.py")
+elif inputtedword[2] == alphabet[12]:
+													execfile("mfile.py")
+elif inputtedword[2] == alphabet[13]:
+														execfile("nfile.py")
+elif inputtedword[2] == alphabet[14]:
+															execfile("ofile.py")
+elif inputtedword[2] == alphabet[15]:
+																execfile("pfile.py")
+elif inputtedword[2] == alphabet[16]:
+																	execfile("qfile.py")
+elif inputtedword[2] == alphabet[17]:
+																		execfile("rfile.py")
+elif inputtedword[2] == alphabet[18]:
+																			execfile("sfile.py")
+elif inputtedword[2] == alphabet[19]:
+																				execfile("tfile.py")
+elif inputtedword[2] == alphabet[20]:
+																					execfile("ufile.py")
+elif inputtedword[2] == alphabet[21]:
+																						execfile("vfile.py")
+elif inputtedword[2] == alphabet[22]:
+																							execfile("wfile.py")
+elif inputtedword[2] == alphabet[23]:
+																								execfile("xfile.py")
+elif inputtedword[2] == alphabet[24]:
+																									execfile("yfile.py")
+elif inputtedword[2] == alphabet[25]:
+																										execfile("zfile.py")	
+
+
+time.sleep(1)
+if inputtedword[3] == alphabet[0]:
+	execfile("afile.py")
+elif inputtedword[3] == alphabet[1]:
+		execfile("bfile.py")
+elif inputtedword[3] == alphabet[2]:
+			execfile("cfile.py")
+elif inputtedword[3] == alphabet[3]:
+				execfile("dfile.py")
+elif inputtedword[3] == alphabet[4]:
+					execfile("efile.py")
+elif inputtedword[3] == alphabet[5]:
+						execfile("ffile.py")
+elif inputtedword[3] == alphabet[6]:
+							execfile("gfile.py")
+elif inputtedword[3] == alphabet[7]:
+								execfile("hfile.py")
+elif inputtedword[3] == alphabet[8]:
+									execfile("ifile.py")
+elif inputtedword[3] == alphabet[9]:
+										execfile("jfile.py")
+elif inputtedword[3] == alphabet[10]:
+											execfile("kfile.py")
+elif inputtedword[3] == alphabet[11]:
+												execfile("lfile.py")
+elif inputtedword[3] == alphabet[12]:
+													execfile("mfile.py")
+elif inputtedword[3] == alphabet[13]:
+														execfile("nfile.py")
+elif inputtedword[3] == alphabet[14]:
+															execfile("ofile.py")
+elif inputtedword[3] == alphabet[15]:
+																execfile("pfile.py")
+elif inputtedword[3] == alphabet[16]:
+																	execfile("qfile.py")
+elif inputtedword[3] == alphabet[17]:
+																		execfile("rfile.py")
+elif inputtedword[3] == alphabet[18]:
+																			execfile("sfile.py")
+elif inputtedword[3] == alphabet[19]:
+																				execfile("tfile.py")
+elif inputtedword[3] == alphabet[20]:
+																					execfile("ufile.py")
+elif inputtedword[3] == alphabet[21]:
+																						execfile("vfile.py")
+elif inputtedword[3] == alphabet[22]:
+																							execfile("wfile.py")
+elif inputtedword[3] == alphabet[23]:
+																								execfile("xfile.py")
+elif inputtedword[3] == alphabet[24]:
+																									execfile("yfile.py")
+elif inputtedword[3] == alphabet[25]:
+																										execfile("zfile.py")
+
+time.sleep(1)
+if inputtedword[4] == alphabet[0]:
+	execfile("afile.py")
+elif inputtedword[4] == alphabet[1]:
+		execfile("bfile.py")
+elif inputtedword[4] == alphabet[2]:
+			execfile("cfile.py")
+elif inputtedword[4] == alphabet[3]:
+				execfile("dfile.py")
+elif inputtedword[4] == alphabet[4]:
+					execfile("efile.py")
+elif inputtedword[4] == alphabet[5]:
+						execfile("ffile.py")
+elif inputtedword[4] == alphabet[6]:
+							execfile("gfile.py")
+elif inputtedword[4] == alphabet[7]:
+								execfile("hfile.py")
+elif inputtedword[4] == alphabet[8]:
+									execfile("ifile.py")
+elif inputtedword[4] == alphabet[9]:
+										execfile("jfile.py")
+elif inputtedword[4] == alphabet[10]:
+											execfile("kfile.py")
+elif inputtedword[4] == alphabet[11]:
+												execfile("lfile.py")
+elif inputtedword[4] == alphabet[12]:
+													execfile("mfile.py")
+elif inputtedword[4] == alphabet[13]:
+														execfile("nfile.py")
+elif inputtedword[4] == alphabet[14]:
+															execfile("ofile.py")
+elif inputtedword[4] == alphabet[15]:
+																execfile("pfile.py")
+elif inputtedword[4] == alphabet[16]:
+																	execfile("qfile.py")
+elif inputtedword[4] == alphabet[17]:
+																		execfile("rfile.py")
+elif inputtedword[4] == alphabet[18]:
+																			execfile("sfile.py")
+elif inputtedword[4] == alphabet[19]:
+																				execfile("tfile.py")
+elif inputtedword[4] == alphabet[20]:
+																					execfile("ufile.py")
+elif inputtedword[4] == alphabet[21]:
+																						execfile("vfile.py")
+elif inputtedword[4] == alphabet[22]:
+																							execfile("wfile.py")
+elif inputtedword[4] == alphabet[23]:
+																								execfile("xfile.py")
+elif inputtedword[4] == alphabet[24]:
+																									execfile("yfile.py")
+elif inputtedword[4] == alphabet[25]:
+																										execfile("zfile.py")
+
+time.sleep(1)
+if inputtedword[5] == alphabet[0]:
+	execfile("afile.py")
+elif inputtedword[5] == alphabet[1]:
+		execfile("bfile.py")
+elif inputtedword[5] == alphabet[2]:
+			execfile("cfile.py")
+elif inputtedword[5] == alphabet[3]:
+				execfile("dfile.py")
+elif inputtedword[5] == alphabet[4]:
+					execfile("efile.py")
+elif inputtedword[5] == alphabet[5]:
+						execfile("ffile.py")
+elif inputtedword[5] == alphabet[6]:
+							execfile("gfile.py")
+elif inputtedword[5] == alphabet[7]:
+								execfile("hfile.py")
+elif inputtedword[5] == alphabet[8]:
+									execfile("ifile.py")
+elif inputtedword[5] == alphabet[9]:
+										execfile("jfile.py")
+elif inputtedword[5] == alphabet[10]:
+											execfile("kfile.py")
+elif inputtedword[5] == alphabet[11]:
+												execfile("lfile.py")
+elif inputtedword[5] == alphabet[12]:
+													execfile("mfile.py")
+elif inputtedword[5] == alphabet[13]:
+														execfile("nfile.py")
+elif inputtedword[5] == alphabet[14]:
+															execfile("ofile.py")
+elif inputtedword[5] == alphabet[15]:
+																execfile("pfile.py")
+elif inputtedword[5] == alphabet[16]:
+																	execfile("qfile.py")
+elif inputtedword[5] == alphabet[17]:
+																		execfile("rfile.py")
+elif inputtedword[5] == alphabet[18]:
+																			execfile("sfile.py")
+elif inputtedword[5] == alphabet[19]:
+																				execfile("tfile.py")
+elif inputtedword[5] == alphabet[20]:
+																					execfile("ufile.py")
+elif inputtedword[5] == alphabet[21]:
+																						execfile("vfile.py")
+elif inputtedword[5] == alphabet[22]:
+																							execfile("wfile.py")
+elif inputtedword[5] == alphabet[23]:
+																								execfile("xfile.py")
+elif inputtedword[5] == alphabet[24]:
+																									execfile("yfile.py")
+elif inputtedword[5] == alphabet[25]:
+																										execfile("zfile.py")
+
+if len(inputtedword) > 6: print("This engine only has capability to execute six letter words.")
+
+elif len(inputtedword) <= 6: 
+	time.sleep(1)
+print(inputtedword + " has been spelled.")
